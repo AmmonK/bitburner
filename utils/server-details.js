2733c811c745858a06e9220561df8d61396c3afd
Ammon King <ammon.j.king@gmail.com>
2022-01-18 19:13:26 -0700

    Update server-details.js

--- utils/server-details.js
+++ utils/server-details.js
@@ -3,33 +3,15 @@ import { getServerPath } from 'find-server.js';
 
 export async function serverDetails(ns, serverName) {
 
-	let server = serverName;	
-	let maxMoney = await ns.getServerMaxMoney(server);
-	let minSecurity = await ns.getServerMinSecurityLevel(server);
-	let portsRequired = await ns.getServerNumPortsRequired(server);
-	let growTime = await ns.getGrowTime(server);
-	let weakTime = await ns.getWeakenTime(server);
-	let hackTime = await ns.getHackTime(server);
-	let hackLevel = await ns.getServerRequiredHackingLevel(server);
-	let hasRoot = await ns.hasRootAccess(server);
-	let money = await ns.getServerMoneyAvailable(server);
 	let serverPath = await getServerPath(ns,serverName);
 
-	let response = {
-		name: server,
-		maxMoney: maxMoney,
-		minSecurity: minSecurity,
-		portsRequired: portsRequired,
-		growTime: growTime,
-		weakenTime: weakTime,
-		hackTime: hackTime,
-		hackLevel: hackLevel,
-		hasRoot: hasRoot,
-		money: money,
-		path: serverPath,
+	let server = ns.getServer(serverName);
+	return{
+		name: serverName,
+		...server,
+		path: serverPath
 	}
 
-	return response;
 
 }
 
