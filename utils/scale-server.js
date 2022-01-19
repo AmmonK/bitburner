f9144286dbc36f6c0de71c5c162b47319fd9df60
Ammon King <ammon.j.king@gmail.com>
2022-01-18 17:39:38 -0700

    Update scale-server.js

--- utils/scale-server.js
+++ utils/scale-server.js
@@ -1,15 +1,22 @@
+import { copyPackages } from "copyPackages";
+
 /** @param {NS} ns **/
-export async function main(ns) {
-	let serverName = ns.args[0];
+
+export async function main(ns){
+	await upgradeServer(ns,ns.args[0]);
+}
+
+export async function upgradeServer(ns,server) {
+	let serverName = server;
 
 	let maxRam = await ns.getServerMaxRam(serverName);
 	ns.tprint(maxRam);
 
-	let multiplier = await getRamMultiplier(maxRam) +1;
+	let multiplier = await getRamMultiplier(maxRam);
+	let upgradeRam = Math.pow(2,multiplier+1);
 
 	let currentCost = await ns.getPurchasedServerCost(maxRam);		
-	let upgradeCost = await ns.getPurchasedServerCost(Math.pow(2,multiplier));
-
+	let upgradeCost = await ns.getPurchasedServerCost(upgradeRam);
 
 	let  formatter = new Intl.NumberFormat('en-US', {
 	style: 'currency',
@@ -21,19 +28,32 @@ export async function main(ns) {
 	//maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 	});
 
+
+
 	ns.tprint("Current: ",formatter.format(currentCost));
 	ns.tprint("Current Ram: ", maxRam);
 	ns.tprint("Upgrade: ",formatter.format(upgradeCost));
-	ns.tprint("Upgrade Ram: ", Math.pow(2,multiplier));
+	ns.tprint("Upgrade Ram: ", upgradeRam);
 
 	if(ns.getServerMoneyAvailable("home") > upgradeCost){
 		ns.tprint("can afford upgrade");
 
+		let upgradeName = `${serverName}-${upgradeRam}`
+
+		// cleanup old server
+		let killResponse = await ns.kill("attackServer.js","home",serverName.replace("bot-",""));
+		ns.tprint(killResponse);
+		await ns.killall(serverName);
+		let deleteResponse = await ns.deleteServer(serverName);
+		ns.tprint("Deleted: ",serverName,deleteResponse);
+
 		// provision new server
-		// kill all on old server
-		// kill attack script running for old server
-		// scp files to new server
-		// run new attack script		
+		let newServer = await ns.purchaseServer(upgradeName,upgradeRam);
+		ns.tprint("New server: ", newServer);
+		await copyPackages(ns,newServer);
+
+		// run attack using new server
+		ns.exec("attackServer.js","home",1,serverName.replace("bot-",""))
 
 	}
 
@@ -51,4 +71,3 @@ async function getRamMultiplier(ram){
 	return multi;
 
 }
-
